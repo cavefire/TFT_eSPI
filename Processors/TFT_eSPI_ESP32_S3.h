d2f98328fd42bf5f07f47232b5191f37e027a26a
Timo Christeleit <timo.christeleit@cavefire.net>
2024-05-31 15:26:24 +0200

    use littlefs instead of spiffs

--- Processors/TFT_eSPI_ESP32_S3.h
+++ Processors/TFT_eSPI_ESP32_S3.h
@@ -151,7 +151,8 @@ SPI3_HOST = 2
   // Call up the SPIFFS (SPI FLASH Filing System) for the anti-aliased fonts
   #define FS_NO_GLOBALS
   #include <FS.h>
-  #include "SPIFFS.h" // ESP32 only
+  #include <LittleFS.h>
+  #define SPIFFS LittleFS
   #define FONT_FS_AVAILABLE
 #endif
 
